6d7379a2c52eb31aa2cbdf33f7181672b99a4e0b
Nesquick <dupuythibaut@yahoo.fr>
2024-09-29 14:35:03 +0200

    Passage d'élements user au controller

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Security;
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name : 'app_dashboard')]
     public function init(): Response
     {
-        return $this->render('home/dashboard.html.twig', []);
+        $user = $this->getUser();
+
+        return $this->render('home/dashboard.html.twig', [
+            'firstname' => $user->getfirstname(),
+            'lastname' => $user->getlastname()
+        ]);
     }
 }
