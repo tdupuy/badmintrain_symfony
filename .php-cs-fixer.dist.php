206df904278ade7368947be23077e6196e44de37
tdupuy <dupuythibaut@yahoo.fr>
2025-03-30 12:32:19 +0200

    test: configuration php-cs

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,13 +1,22 @@
 <?php
 
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests')
     ->exclude('var')
 ;
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        '@PSR12' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short']
     ])
     ->setFinder($finder)
 ;
